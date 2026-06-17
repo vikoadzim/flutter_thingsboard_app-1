153502f0037ab7939e81c13936f4b291e35e428b
vikoadzim <126139147+vikoadzim@users.noreply.github.com>
2026-06-18 00:16:55 +0700

    Update pagination_list_widget.dart

--- lib/utils/ui/pagination_widgets/pagination_list_widget.dart
+++ lib/utils/ui/pagination_widgets/pagination_list_widget.dart
@@ -34,7 +34,7 @@ class PaginationListWidget<T, B> extends StatelessWidget {
             pagingController: pagingController,
             builderDelegate: builderDelegate,
             separatorBuilder:
-                separatorWidgetBuilder ?? (_, _) => const SizedBox(height: 16),
+                separatorWidgetBuilder ?? (_, _1) => const SizedBox(height: 16),
           ),
         ),
       ],
